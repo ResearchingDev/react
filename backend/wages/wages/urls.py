c47bcaaca2535a811a9b4a17caa42165b5a225a4
Kalaivani BUI0042 <Kalaivani@brightuitechnologies.com>
2024-12-26 18:10:33 +0530

    Wages-Payroll-HRM/Dev/Signup Page - Cors Policy and Axios Plugin Setups

--- backend/wages/wages/urls.py
+++ backend/wages/wages/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from wages.server.signup.views import SignupAPIView
+from server.signup.views import SignupAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
