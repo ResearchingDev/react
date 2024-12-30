f7b98bf4c40faec34b9e758a71879832394b6fef
Balasundram BUI0011 <bala@brightuitechnologies.com>
2024-12-30 15:42:18 +0530

    Sidebar width UI working, Manage User Role static design working

--- frontend/src/components/Breadcrumbs/Breadcrumb.tsx
+++ frontend/src/components/Breadcrumbs/Breadcrumb.tsx
@@ -4,9 +4,14 @@ interface BreadcrumbProps {
 }
 const Breadcrumb = ({ pageName }: BreadcrumbProps) => {
   return (
-    <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
+    <div className="mb-4 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
       <h2 className="text-title-md2 font-semibold text-black dark:text-white">
         {pageName}
+        <Link
+          href="#"
+          className="add-btn inline-flex rounded-md bg-primary text-white hover:bg-opacity-90">
+          Add
+        </Link>
       </h2>
 
       <nav>
--- frontend/src/components/Sidebar/index.tsx
+++ frontend/src/components/Sidebar/index.tsx
@@ -83,6 +83,7 @@ const menuGroups = [
         route: "#",
         children: [
           { label: "Manage Users", route: "/users/manage-users" },
+          { label: "Manage Users Role", route: "/users/manage-user-role" },
           { label: "Add User", route: "/users/add-user" },
         ],
       },
@@ -340,12 +341,12 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
   return (
     <ClickOutside onClick={() => setSidebarOpen(false)}>
       <aside
-        className={`fixed left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-black duration-300 ease-linear dark:bg-boxdark lg:translate-x-0 ${
+        className={`fixed left-0 top-0 z-9999 flex h-screen w-60 flex-col overflow-y-hidden bg-black duration-300 ease-linear dark:bg-boxdark lg:translate-x-0 ${
           sidebarOpen ? "translate-x-0" : "-translate-x-full"
         }`}
       >
         {/* <!-- SIDEBAR HEADER --> */}
-        <div className="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5">
+        <div className="flex items-center justify-between gap-2 px-2 py-2 lg:py-4 bg-black-top">
           <Link href="/">
             <Image
               width={176}
@@ -380,13 +381,9 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
 
         <div className="no-scrollbar flex flex-col overflow-y-auto duration-300 ease-linear">
           {/* <!-- Sidebar Menu --> */}
-          <nav className="mt-5 px-4 py-4 lg:mt-9 lg:px-6">
+          <nav className="lg:mt-2 lg:px-2">
             {menuGroups.map((group, groupIndex) => (
               <div key={groupIndex}>
-                <h3 className="mb-4 ml-4 text-sm font-semibold text-bodydark2">
-                  {group.name}
-                </h3>
-
                 <ul className="mb-6 flex flex-col gap-1.5">
                   {group.menuItems.map((menuItem, menuIndex) => (
                     <SidebarItem
